835cf23ec5a385ca07bd65310ef9f7b50f3eb761
Crespo Wang <crespo.wang@servian.com.au>
2016-12-20 08:55:08 +1100

    trying to get configure filebeat

--- filebeat/run.sh
+++ filebeat/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# curl --silent http://stream.meetup.com/2/rsvps >> /var/log/rsvps.log
+
 sleep 10000
+curl -o /var/log/rsvps/rsvps-`date +'%Y%m%d'`.log --silent http://stream.meetup.com/2/rsvps
